b3c439bb4a5e01c9e807a4e334aa9b4f4b55dda0
Cyril Isip <cisip@Cyrils-MacBook-Pro.local>
2021-10-11 20:35:58 -0500

    app is running and uploaded on github

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 4001;
+
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+// use to parse data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
